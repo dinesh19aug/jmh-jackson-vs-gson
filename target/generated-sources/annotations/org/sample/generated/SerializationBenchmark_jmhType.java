eb8655b94019883241b823c31dadafd7c04aa043
BEAST-PC\dines <dinesh19aug@gmail.com>
2023-08-27 16:10:47 -0400

    Removed target folder

--- target/generated-sources/annotations/org/sample/generated/SerializationBenchmark_jmhType.java
+++
@@ -1,4 +0,0 @@
-package org.sample.generated;
-public class SerializationBenchmark_jmhType extends SerializationBenchmark_jmhType_B3 {
-}
-
--- target/generated-sources/annotations/org/sample/generated/SerializationDataProvider_jmhType.java
+++
@@ -1,4 +0,0 @@
-package org.sample.generated;
-public class SerializationDataProvider_jmhType extends SerializationDataProvider_jmhType_B3 {
-}
-
